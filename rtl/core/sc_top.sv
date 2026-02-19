744ba5297fd74353eadc93853360a5c90e58bd4c
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-02-19 16:47:32 -0300

    Refactor sc_top module to include new inputs and bus

--- rtl/core/sc_top.sv
+++ rtl/core/sc_top.sv
@@ -1,14 +1,37 @@
-module charge_supervised_tb(input logic clk, logic reset_n);
+// sc_top.sv
+module sc_top
+(
+input logic clk, 
+input logic reset_n,
+input logic battery_connected,
+input logic battery_full,
+input logic ml_predict_instability,
+input grid_state_t grid_state
+);
+
+	sc_interface_if sc_bus();
+	state_t current_state;
 	
-	charge_supervised u_charge
+	//assignments
+	always_comb begin
+  	    sc_bus.battery_connected = battery_connected;
+	    sc_bus.battery_full = battery_full;
+	end
+	
+	sc_fsm u_fsm
 	(
-	   .clk(clk),
-	   .reset_n(reset_n),
-	   .grid_status(grid_status),
-	   .battery_connected(battery_connected),
-	   .charge_complete(charge_complete),
-	   .load_request(load_request),
-	   .ml_predict_instability(ml_predict_instability),
+	  .clk(clk),
+	  .reset_n(reset_n),
+	  .fsm_bus(sc_bus),
+	  .current_state(current_state)
+	);
+	
+	sc_safety_monitor u_safety
+	(
+	  .clk(clk),
+	  .reset_n(reset_n),
+	  .ml_predict_instability(ml_predict_instability),
+	  .safety_bus(safety_bus)
 	);
 	
 endmodule
