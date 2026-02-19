9644362f5c546fe6de6da573230d3c39010b3765
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-02-19 16:45:52 -0300

    Updating fsm file

--- rtl/core/sc_fsm.sv
+++ rtl/core/sc_fsm.sv
@@ -1,5 +1,5 @@
 //global include from source include file
-`include "include/sc_include.svh"
+`include "rtl/include/sc_include.svh"
 
 module sc_fsm
 (
@@ -7,38 +7,31 @@ module sc_fsm
   input logic clk,
   input logic reset_n,
   
-  //come from grid_classifier
-  input grid_state_t grid_state,
-  
-  //come from load
-  input logic battery_connected,
-  input logic battery_full,
-  
-  //come from safety
-  input logic fault_flag,
-  
   //going to actuator
-  output logic charge_enable,
-  output logic relay_activation,
-  output state_t current_state
+  sc_interface_if.fsm_connect fsm_bus
 );
 
+import sc_types_pkg::*;
 //O grid_state só é olhado no carregamento e no check_grid
 
-reg [2:0] next_state;
+grid_state_t grid_state_enum;
+
+assign grid_state_enum = grid_state_t'(fsm_bus.grid_state);
+
+state_t next_state;
 
 always_comb begin
    //default
-   next_state = current_state;
+   next_state = fsm_bus.current_state;
+   fsm_bus.charge_enable = 0;
+   fsm_bus.relay_activation = 0;
    
-   case(current_state) begin
-   	charge_enable = 1'b0;
-   	relay_activation = 1'b0;
+   case(fsm_bus.current_state)
    	
 	IDLE: begin
-	   if(battery_connected && !battery_full) begin
-	   	if(current_state == GRID_CRITICAL) begin
-	     	  fault_flag = 1'b1;
+	   if(fsm_bus.battery_connected && !fsm_bus.battery_full) begin
+	   	if(grid_state_enum == GRID_CRITICAL) begin
+	     	  //fault_flag = 1'b1;
 	    	  next_state = FAULT;
 	   	end
 	   	else begin
@@ -48,35 +41,39 @@ always_comb begin
 	end
 	
 	CHECK_GRID: begin
-	   if(fault_flag) begin
+	   if(grid_state_enum == GRID_CRITICAL) begin
 	   	next_state = FAULT;
 	   end
 	   
-	   else if(grid_state == GRID_CRITICAL) begin
-	   	next_state = FAULT;
-	   end
-	   
-	   else if(grid_state == GRID_UNSTABLE) begin
+	   else if(grid_state_enum == GRID_UNSTABLE) begin
 	   	next_state = WAIT;
 	   end
 	   else begin
-	   	charge_enable = 1'b1;
+	   	fsm_bus.charge_enable = 1'b1;
 		next_state = CHARGING;
 	   end
 	end
 	
 	CHARGING: begin
-	   relay_activation = 1'b1;
+	   fsm_bus.relay_activation = 1'b1;
+	   fsm_bus.charge_enable = 1'b1;
 	   
-	   if(battery_full) begin
-	   	relay_activation = 1'b0;
-	   	charge_enable = 1'b0;
+	   if(fsm_bus.battery_full) begin
+	   	fsm_bus.relay_activation = 1'b0;
+	   	fsm_bus.charge_enable = 1'b0;
 	   	
 	   	next_state = IDLE;
 	   end
+	   
+	   else if(grid_state_enum == GRID_UNSTABLE) begin
+	   	fsm_bus.relay_activation = 1'b0;
+	   	fsm_bus.charge_enable = 1'b0;
+	   	next_state = WAIT;
+	   end
+	   
 	end
 	WAIT: begin
-	    if(grid_state == GRID_NORMAL) begin 
+	    if(grid_state_enum == GRID_NORMAL) begin 
 	   	next_state = IDLE;
 	    end
 	end
@@ -85,20 +82,32 @@ always_comb begin
 	    if(reset_n) next_state = IDLE;
 	end
 	
-	default: next_state = IDLE;
-   end
-   
+	default: begin 
+		next_state = FAULT;
+	end
+   endcase
    
 end
 
+/*
 always_ff @(posedge clk or negedge reset_n) begin
-   if(!reset_n) begin
-   	current_state <= IDLE;
+   if(reset_n) begin
+   	charge_enable_r <= 0;
+   	relay_activation_r <= 0;
    end
    else begin
-   	current_state <= next_state;
+   	fsm_bus.charge_enable <= charge_enable_r;
+   	fsm_bus.relay_activation <= relay_activation_r;
    end
    
+end
+*/
+// registrador de estado
+always_ff @(posedge clk or negedge reset_n) begin
+  if (!reset_n)
+    fsm_bus.current_state <= IDLE;
+  else
+    fsm_bus.current_state <= next_state;
 end
 
 endmodule
