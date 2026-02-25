79d83b447e0d205eed007b8c4d6079807fe4bcaa
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-02-25 03:36:35 -0300

    Adding files from formal directory
    Adding assertion code to avoid wrong state flow, which isn't specified in FSM roadmap.

---
+++ formal/sc_assertions.sv
@@ -0,0 +1,31 @@
+module sc_assertions (input logic clk, sc_interface_if fsm_bus);
+
+import sc_types_pkg::*;
+
+/*the next state cannot be CHARGING state*/
+a_assert: assert property (@(posedge clk)
+    (fsm_bus.current_state == IDLE) |=> (fsm_bus.current_state != CHARGING)
+);
+	
+//the battery full to condition to change the state
+b_assert: assert property (@(posedge clk)
+	(fsm_bus.current_state == CHARGING && (fsm_bus.battery_full || !fsm_bus.battery_connected))
+	|=>
+	(fsm_bus.current_state != CHARGING)
+	);
+	
+//ensuring the FAULT transaction state
+c_assert: assert property (@(posedge clk)
+	(fsm_bus.current_state == FAULT && (fsm_bus.battery_connected || !reset_n) )
+	|=>
+	(fsm_bus.current_state != CHARGING && fsm_bus.current_state != CHECK_GRID && fsm_bus.current_state != WAIT)
+	);
+
+//Cannot go to undesired state from WAIT
+d_assert: assert property (@(posedge clk)
+	(fsm_bus.current_state == WAIT)
+	|=>
+	(fsm_bus.current_state != IDLE && fsm_bus.current_state != FAULT && fsm_bus.current_state != CHECK_GRID)
+	);
+	
+endmodule
