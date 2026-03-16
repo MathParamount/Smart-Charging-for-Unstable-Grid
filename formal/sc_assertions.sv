164b9aa6137aaba7d94a09be1add6f10f218cb4f
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-03-15 21:07:41 -0300

    Update sc_assertions.sv

--- formal/sc_assertions.sv
+++ formal/sc_assertions.sv
@@ -14,24 +14,4 @@ b_assert: assert property (@(posedge clk)
 	(fsm_bus.current_state != CHARGING && fsm_bus.current_state != CHECK_GRID)
 	);
 	
-//ensuring the FAULT transaction state
-c_assert: assert property (@(posedge clk)
-	(fsm_bus.current_state == FAULT && (fsm_bus.battery_connected || !reset_n) )
-	|=>
-	(fsm_bus.current_state != CHARGING && fsm_bus.current_state != CHECK_GRID && fsm_bus.current_state != WAIT)
-	);
-
-//Cannot go to undesired state from WAIT
-d_assert: assert property (@(posedge clk)
-	(fsm_bus.current_state == WAIT)
-	|=>
-	(fsm_bus.current_state != IDLE && fsm_bus.current_state != FAULT && fsm_bus.current_state != CHECK_GRID)
-	);
-	
-e_assert: assert property (@(posedge clk)
-	!(fsm_bus.current_state inside {IDLE,CHECK_GRID,CHARGING,WAIT,FAULT})
-	|=>
-	fsm_bus.current_state == IDLE
-	);
-	
 endmodule
