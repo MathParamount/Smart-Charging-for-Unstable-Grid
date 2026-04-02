3351f8e70f0e7db470515a983dc6ec42bd758ac4
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-04-01 21:36:04 -0300

    Refactor fault flag and code logic in safety monitor

--- rtl/core/sc_safety_monitor.sv
+++ rtl/core/sc_safety_monitor.sv
@@ -12,82 +12,98 @@ module sc_safety_monitor
 	//combinational logic to state conditions
 	always_comb begin
 		//default
-		safety_bus.fault_flag = 1'b0;
-		safety_bus.fault_code = 4'b0;
+		safety_bus.fault_flag = 0;
+		safety_bus.fault_code = '0;
 		
-		case(safety_bus.current_state)
+		grd_saft = grid_state_t'(safety_bus.grid_state);
+		
+		unique case(safety_bus.current_state)
+			
 			IDLE: begin
-				safety_bus.fault_flag = 0;
-				safety_bus.fault_code = '0;
+			   safety_bus.fault_flag = 0;
+			   safety_bus.fault_code = 2'b00;
 			end
 			
 			CHECK_GRID: begin
-			    case(safety_bus.grid_state)
+			    case(grd_saft)
 				GRID_CRITICAL: begin
-				    safety_bus.fault_flag = 1'b1;
-				    safety_bus.fault_code = 4'b0001;
+				    safety_bus.fault_flag = 1;
+				    safety_bus.fault_code = 2'b11;
 				end
 				
 				GRID_UNSTABLE: begin
-				    safety_bus.fault_code = 4'b0010;
+				    safety_bus.fault_flag = 0;
+				    safety_bus.fault_code = 2'b10;
 				end
 				
 				GRID_NORMAL: begin 
-				   safety_bus.fault_code = 4'b0011;			
+				    safety_bus.fault_flag = 0;
+				    safety_bus.fault_code = 2'b01;			
 				end
 				default: begin
-				   safety_bus.fault_flag = 1'b1;
-				   safety_bus.fault_code = '0;			
+				    safety_bus.fault_flag = safety_bus.ml_predict_instability;
 				end
 			    endcase
-			    
-			   	if(safety_bus.ml_predict_instability) begin
-			   	    safety_bus.fault_flag = 1'b1;
-			   	    safety_bus.fault_code = 4'b0100;
-			   	end
 			end
 
 			CHARGING: begin
 				//grid verification
-				
-				case(safety_bus.grid_state)
+				case(grd_saft)
 				   GRID_CRITICAL: begin
 				     safety_bus.fault_flag = 1'b1;
-				     safety_bus.fault_code = 4'b0101;
+				     safety_bus.fault_code = 2'b11;
 				   end
 				   GRID_UNSTABLE: begin
-					safety_bus.fault_code = 4'b0110;
+					safety_bus.fault_flag = 1'b0;
+				        safety_bus.fault_code = 2'b10;
 				   end
 				   GRID_NORMAL: begin
-					safety_bus.fault_code = 4'b0111;
+				    safety_bus.fault_flag = 1'b0;
+				    safety_bus.fault_code = 2'b01;
 				   end
 				   
 				   default: begin
-				   	safety_bus.fault_flag = 0;
+					safety_bus.fault_flag = safety_bus.ml_predict_instability;
 				   end
-			        endcase
+			    	endcase
 			end
 			
 			WAIT: begin
-			
-			   if(safety_bus.grid_state == GRID_NORMAL && !safety_bus.ml_predict_instability)
+			    
+			   if(grd_saft == GRID_UNSTABLE || grd_saft == GRID_NORMAL)
 			   begin
 				safety_bus.fault_flag = 0;
+				safety_bus.fault_code = 2'b10;
 			   end
 			end
 
 			FAULT: begin
-				if(reset_n) begin
-					safety_bus.fault_code = '0;
-					safety_bus.fault_flag = 0;
+				if(grd_saft == GRID_CRITICAL) begin
+				   safety_bus.fault_flag = 1;
+				   safety_bus.fault_code = 2'b11;
+				end
+				
+				else if(grd_saft == GRID_NORMAL) begin
+				   safety_bus.fault_flag = 1'b0;
+				   safety_bus.fault_code = 2'b01;
+				end
+				
+				else if(grd_saft == GRID_UNSTABLE) begin
+				   safety_bus.fault_flag = 1'b0;
+				   safety_bus.fault_code = 2'b10;
+				end
+				
+				if(!reset_n) begin
+				    safety_bus.fault_flag = 0;
 				end
 			end
 			
 			default: begin
-				safety_bus.fault_flag = 0;
+				safety_bus.fault_flag = safety_bus.ml_predict_instability;
 			end
-		endcase
+	       endcase
 	end
+	
 	
 	//outputs(assigns)
 	
