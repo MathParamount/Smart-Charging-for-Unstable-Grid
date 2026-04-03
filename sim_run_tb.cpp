670c3ffb4568ee39277099a46ec3ecc433fd9c77
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-04-03 18:06:21 -0300

    Updating cpp file to fix bugs
    I made changes in sim file which is responsible to generate the wave form and simulate some issues that turns out the project more easy to analyze and scalable.

--- sim_run_tb.cpp
+++ sim_run_tb.cpp
@@ -8,15 +8,86 @@ using namespace std;
 
 vluint64_t main_t = 0;
 
-void tick(Vsc_dut *top)
+VerilatedVcdC* pointer = nullptr;
+Vsc_dut* top = nullptr;
+
+const vluint64_t clock_half_period = 10000;	//metade de 10ns
+
+vluint64_t main_time = 0;	//to catch time unit of clock debug
+
+//clock count, time unit count and generation waves
+void tick(Vsc_dut *top, VerilatedVcdC* pointer)
 {
 	top->clk = 0;
 	top->eval();
-   main_t++;
+	
+	main_t += clock_half_period;	//forward 10 ns
+
+	pointer->dump(main_t);	//dump in clk=0
 
 	top->clk=1;
 	top->eval();
-	main_t++;
+	
+	main_t += clock_half_period;	//forward 10 ns
+
+	pointer->dump(main_t);	//dump in clk=1
+}
+
+//function to test the system 
+void test_function(Vsc_dut *top)
+{
+  top->battery_full = 0;
+  top->relay_active = 0;
+  top->charge_enable = 0;
+  top->grid_state = 0;
+  top->fault_flag = 0;
+  top->fault_code = 0;
+  
+  tick(top,pointer);
+
+  while(main_t < 20000000)
+  {
+  		top->battery_connected = 1;
+	
+  		if(main_t <= 50000)
+  		{
+		/*
+    	top->relay_active = 0;
+	 	top->charge_enable = 0;
+		top->fault_flag = 0;
+	 	*/
+		top->grid_state = 0;
+  		}
+  		else if(main_t <= 100000)
+  		{
+	 	//top->relay_active = 0;
+	 	top->grid_state = 1;
+  		}
+  		else if(main_t <= 150000)
+  		{
+	 	/*
+		top->charge_enable = 1;
+	 	top->relay_active = 1;
+	 	*/
+		top->grid_state = 2;
+  		}
+		else if( main_t <= 250000)
+		{
+			top->battery_full = 0;
+			top->grid_state = 0;
+		}
+		else if (main_t <= 350000)
+		{
+		 top->ml_predict_instability=1;
+		}
+		else
+		{
+		top->ml_predict_instability = 0;
+		}
+		
+		//clock generation
+		tick(top,pointer);
+	}
 }
 
 int main(int argc, char **argv)
@@ -24,29 +95,32 @@ int main(int argc, char **argv)
   Verilated::commandArgs(argc,argv);
   Verilated::traceEverOn(true);
   
-  Vsc_dut *top = new Vsc_dut;
-  VerilatedVcdC* pointer = new VerilatedVcdC;
+  top = new Vsc_dut;
+  pointer = new VerilatedVcdC;
   
   top->trace(pointer,99);
   pointer->open("wave.vcd");
   
+
   cout << "---------DEVICE_SIMULATION-----------" << endl;
-  
+
   top->clk = 0;
   top->reset_n = 0;
 
-//
+// reset test during 5 clock cycles
   for(int i =0; i< 5; i++)
   {
 		top->clk = !top->clk;
 		top->eval();
 		
-		pointer->dump(main_t*10 + 5);
+		main_t += clock_half_period;
+
+		pointer->dump(main_t);
 
 		!(top->reset_n) ? cout << "reset is active" << endl : cout << "isn't active" << endl;
   }
 
-  cout << "Actual time count: " << main_t << endl;
+  cout << "Actual time count: " << time << endl;
 
 //
 
@@ -65,54 +139,9 @@ int main(int argc, char **argv)
 
   tick(top,pointer);		//stabilization of 1 cycle
   
-  //system test
-  
-  top->battery_full = 0;
-  top->relay_active = 0;
-  top->charge_enable = 0;
-  top->grid_state = 0;
-  tick(top,pointer);
-	
-  while(main_t < 50)
-  {
-  		top->battery_connected = 1;
-	
-  		if(main_t <= 10)
-  		{
-		/*
-    	top->relay_active = 0;
-	 	top->charge_enable = 0;
-		top->fault_flag = 0;
-	 	*/
-		top->grid_state = 0;
-  		}
-  		else if(main_t <= 20)
-  		{
-	 	//top->relay_active = 0;
-	 	top->grid_state = 1;
-  		}
-  		else if(main_t <= 30)
-  		{
-	 	/*
-		top->charge_enable = 1;
-	 	top->relay_active = 1;
-	 	*/
-		top->grid_state = 2;
-  		}
-  		else
-  		{
-	 	/*
-		top->relay_active = 0;
-	 	top->charge_enable =0;
-		top->fault_flag = 1;
-	 	top->battery_full = 1;
-		*/
-	 	top->grid_state = 4;
-  		}
-
-		//clock generation
-		tick(top,pointer);
-	}
+  //system test function
+  test_function(top);
+ 
   //top->grid_state = 0;	//Grid state(manual)
 
   top->ml_predict_instability = 0;
@@ -122,17 +151,20 @@ int main(int argc, char **argv)
   cout << "grid state: " << bitset<3> (top->grid_state) << endl;
   cout << "Battery full: " << (int)top->battery_full << endl;
   cout << "ml predict: " << (int)top->ml_predict_instability << endl;
-   
+  cout << "fault_code: " << (int)top->fault_code << endl;
+
   cout << "---------------FINAL_TEST-----------------" << endl;
 
-  for(int time = 0; time < 1000; time++)
+  for(int time = 0; time < 10000; time++)
   {
      top->clk = !top->clk;
      top->eval();
      
+	  main_time += 1;	//1 time unit
+
      if(time%100 == 0) 
 	  {
-		  cout << "time step: " << time << endl;
+		  cout << "time step: " << main_time << endl;
   	  }
   }
    
@@ -144,3 +176,4 @@ int main(int argc, char **argv)
   
   return 0;
 }
+
