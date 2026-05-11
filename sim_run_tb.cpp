ccb8b2d514210b80a789162039bf19dad2142862
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-05-10 21:17:29 -0300

    Refactor simulation time handling and reset logic

--- sim_run_tb.cpp
+++ sim_run_tb.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 VerilatedVcdC* pointer = nullptr;
 Vsc_dut* top = nullptr;
 
@@ -17,22 +16,10 @@ vluint64_t main_t = 0;
 // Prototype
 void tick(Vsc_dut *top, VerilatedVcdC* pointer);
 void set_voltage(Vsc_dut *top, double V_rms);
-=======
-vluint64_t main_t = 0;
-
-VerilatedVcdC* pointer = nullptr;
-Vsc_dut* top = nullptr;
-
-const vluint64_t clock_half_period = 10000;	//metade de 10ns
-
-vluint64_t main_time = 0;	//to catch time unit of clock debug
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 
 //clock count, time unit count and generation waves
 void tick(Vsc_dut *top, VerilatedVcdC* pointer)
 {
-<<<<<<< HEAD
-
  cout << "tick start, time=" << main_t << endl;
 
     top->clk = 0;
@@ -104,81 +91,6 @@ void test_step(Vsc_dut *top)
 	 printf("Time=%lu, V_rms=%.1f, ADC=%d\n", main_t, V_rms, top->grid_voltage_adc);
 }
 
-
-=======
-	top->clk = 0;
-	top->eval();
-	
-	main_t += clock_half_period;	//forward 10 ns
-
-	pointer->dump(main_t);	//dump in clk=0
-
-	top->clk=1;
-	top->eval();
-	
-	main_t += clock_half_period;	//forward 10 ns
-
-	pointer->dump(main_t);	//dump in clk=1
-}
-
-//function to test the system 
-void test_function(Vsc_dut *top)
-{
-  top->battery_full = 0;
-  top->relay_active = 0;
-  top->charge_enable = 0;
-  top->grid_state = 0;
-  top->fault_flag = 0;
-  top->fault_code = 0;
-  
-  tick(top,pointer);
-
-  while(main_t < 20000000)
-  {
-  		top->battery_connected = 1;
-	
-  		if(main_t <= 50000)
-  		{
-		/*
-    	top->relay_active = 0;
-	 	top->charge_enable = 0;
-		top->fault_flag = 0;
-	 	*/
-		top->grid_state = 0;
-  		}
-  		else if(main_t <= 100000)
-  		{
-	 	//top->relay_active = 0;
-	 	top->grid_state = 1;
-  		}
-  		else if(main_t <= 150000)
-  		{
-	 	/*
-		top->charge_enable = 1;
-	 	top->relay_active = 1;
-	 	*/
-		top->grid_state = 2;
-  		}
-		else if( main_t <= 250000)
-		{
-			top->battery_full = 0;
-			top->grid_state = 0;
-		}
-		else if (main_t <= 350000)
-		{
-		 top->ml_predict_instability=1;
-		}
-		else
-		{
-		top->ml_predict_instability = 0;
-		}
-		
-		//clock generation
-		tick(top,pointer);
-	}
-}
-
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 int main(int argc, char **argv)
 {
   Verilated::commandArgs(argc,argv);
@@ -193,7 +105,6 @@ int main(int argc, char **argv)
 
   cout << "---------DEVICE_SIMULATION-----------" << endl;
 
-<<<<<<< HEAD
   // Reset
   main_t = 0;
   
@@ -296,80 +207,4 @@ int main(int argc, char **argv)
 
   	 cout << "End simulation" << endl;
   	 return 0;
- 
 }
-=======
-  top->clk = 0;
-  top->reset_n = 0;
-
-// reset test during 5 clock cycles
-  for(int i =0; i< 5; i++)
-  {
-		top->clk = !top->clk;
-		top->eval();
-		
-		main_t += clock_half_period;
-
-		pointer->dump(main_t);
-
-		!(top->reset_n) ? cout << "reset is active" << endl : cout << "isn't active" << endl;
-  }
-
-  cout << "Actual time count: " << time << endl;
-
-//
-
-  cout << "---------------SIGNAL_TEST-----------------" << endl;
-  
-  top->clk = 0;
-  top->reset_n = 0;
-
-  //pointer->dump(main_t*10);	//manual dump for choice
-
-  /*
-  for(int i =0; i < 5; i++) tick(top,pointer);	 //Detect 5 clock stimulus
-  */
-
-  top->reset_n = 1;
-
-  tick(top,pointer);		//stabilization of 1 cycle
-  
-  //system test function
-  test_function(top);
- 
-  //top->grid_state = 0;	//Grid state(manual)
-
-  top->ml_predict_instability = 0;
-
-  //Verify result
-  cout << "battery connected: " << (bool)top->battery_connected << endl;
-  cout << "grid state: " << bitset<3> (top->grid_state) << endl;
-  cout << "Battery full: " << (int)top->battery_full << endl;
-  cout << "ml predict: " << (int)top->ml_predict_instability << endl;
-  cout << "fault_code: " << (int)top->fault_code << endl;
-
-  cout << "---------------FINAL_TEST-----------------" << endl;
-
-  for(int time = 0; time < 10000; time++)
-  {
-     top->clk = !top->clk;
-     top->eval();
-     
-	  main_time += 1;	//1 time unit
-
-     if(time%100 == 0) 
-	  {
-		  cout << "time step: " << main_time << endl;
-  	  }
-  }
-   
-  pointer->close();
-  delete pointer;
-  
-  delete top;
-  cout << "End simulation" << endl;
-  
-  return 0;
-}
-
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
