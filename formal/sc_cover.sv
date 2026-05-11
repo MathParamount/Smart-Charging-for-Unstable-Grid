8a4ef0694f4a9eeede5f719b2b44ea898590e28a
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-05-10 21:19:32 -0300

    Remove redundant state cover properties from sc_cover

--- formal/sc_cover.sv
+++ formal/sc_cover.sv
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module sc_cover(input logic clk, sc_interface_if fsm_bus);
 
 import sc_types_pkg::*;
@@ -34,39 +33,3 @@ import sc_types_pkg::*;
  );
  
 endmodule
-=======
-module sc_cover(input logic clk, sc_interface_if fsm_bus);
-
-import sc_types_pkg::*;
-
- //cover IDLE state
- a_cover: cover property(@(posedge clk)
-    (fsm_bus.current_state == IDLE) |=> (fsm_bus.current_state != CHARGING)
- );
- 
- //cover charging state
- b_cover: cover property(@(posedge clk)
-    (fsm_bus.current_state == CHARGING && (fsm_bus.battery_full || !fsm_bus.battery_connected))
-    |=>
-    (fsm_bus.current_state != CHARGING && fsm_bus.current_state != CHECK_GRID)
- );
-
-  //cover Fault current state
- c_cover: cover property(@(posedge clk)
-   (fsm_bus.current_state == FAULT && (fsm_bus.battery_connected || !reset_n) )
-   |=>
-   (fsm_bus.current_state != CHARGING && fsm_bus.current_state != CHECK_GRID && fsm_bus.current_state != WAIT)
- );
- 
- d_cover: cover property(@(posedge clk)
-   (fsm_bus.current_state == WAIT)
-   |=>
-   (fsm_bus.current_state != IDLE && fsm_bus.current_state != FAULT && fsm_bus.current_state != CHECK_GRID)
- );
- 
- //covering exceptional state
- e_cover: cover property(@(posedge clk)
-    fsm_bus.current_state == CHECK_GRID
- );
-endmodule
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
