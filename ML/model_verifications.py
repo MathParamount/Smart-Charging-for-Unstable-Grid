095f541e2488854957ff8dfc2f1627223de99215
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-05-10 21:23:47 -0300

    Resolve merge conflict in model_verifications.py

--- ML/model_verifications.py
+++ ML/model_verifications.py
@@ -2,16 +2,10 @@ from data_box_cox_f import *
 from regression_code import *
 
 #Global critical voltage (STM32 referc)
-<<<<<<< HEAD
 v_critico_low_adc = 0.83 * v_f.target
 v_unstable_min_adc = 0.95 * v_f.target
 
 v_critico_high_adc = 1.15 * v_f.target
-=======
-v_critico_low_adc = 1.2
-v_critico_high_adc = 2.8
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
-
 
 class verification_model:
 	def verf(self,reg_f):
@@ -34,11 +28,7 @@ class verification_model:
 		print(f"coefficient: {reg_f.coeffc}")
 		print(f"intercept: {reg_f.interc}")
         
-<<<<<<< HEAD
       #prediction plot
-=======
-        #prediction plot
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 		plt.subplot(1,2,1)
 		plt.title("Voltage prediction X original")
 		plt.scatter(reg_f.y_original_new,reg_f.prediction,alpha = 0.3)
@@ -53,16 +43,10 @@ class verification_model:
 		
 
 		#plot of residuo x prediction
-<<<<<<< HEAD
 		plt.title("residue x prediction")
 		sea.histplot(data = v_f.df_new, x= reg_f.resd)
 		#plt.scatter(reg_f.resd, reg_f.prediction)
 		#plt.axhline(y=0, color = 'r', linestyle = '--')
-=======
-		plt.title("residuo x prediction")
-		plt.scatter(reg_f.resd, reg_f.prediction)
-		plt.axhline(y=0, color = 'r', linestyle = '--')
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 		plt.show()
 		
 		#plot prediction x time
@@ -77,8 +61,6 @@ class verification_model:
 		ax2.set_title("Voltage x time")
 		plt.tight_layout()      #Spacement adjust
 		plt.show()
-<<<<<<< HEAD
-
 
 		#model validation
 		v_measured = reg_f.prediction - v_f.target[679:];
@@ -89,8 +71,6 @@ class verification_model:
 		plt.plot(v_measured)
 		plt.axhline(0, color='red', linestyle='--')     #Line zero erro
 		plt.show()
-=======
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 		
 		#overfitting test
 		diff_modl = abs(reg_f.train_score - reg_f.test_score)
@@ -100,7 +80,6 @@ class verification_model:
   
 
 	def result(self,reg_f):
-<<<<<<< HEAD
 		#Verify if the prediction is bellow of any prediction values
 		
 		if (reg_f.prediction[:,None] < v_critico_low_adc.values).any():
@@ -125,25 +104,6 @@ class verification_model:
 		print(f"\nvoltage prediction: {output_hardw}, condition: {action}, instability_level: {instability_level}")
 
 		return output_hardw
-=======
-		v_measured = reg_f.prediction - v_f.target[679:];
-
-		#plot instability
-		plt.title("prediction - voltage")
-		plt.suptitle("voltage stability measurement", fontsize= 16)
-		plt.plot(v_measured)
-		plt.show()
-
-		if (v_measured < v_critico_low_adc).any() or (v_measured > v_critico_high_adc).any():
-			self.ml_predict_instability = 1;
-		else:
-			self.ml_predict_instability = 0;
-
-		print(f"predict critical instability: {self.ml_predict_instability}")
-
-		return self.ml_predict_instability
-
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 
 df3 = pd.read_csv('dataset/smart_grid_dataset.csv')
 v_f = voltage_filter(df3)
