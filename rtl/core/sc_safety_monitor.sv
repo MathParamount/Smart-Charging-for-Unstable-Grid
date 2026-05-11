90208bfd5d0342a57bdf380aee1be064b99e6d53
Gabriel da cruz <154000020+MathParamount@users.noreply.github.com>
2026-05-10 21:07:38 -0300

    Solving the git bug issues 2

--- rtl/core/sc_safety_monitor.sv
+++ rtl/core/sc_safety_monitor.sv
@@ -1,5 +1,5 @@
 //`timeunit 1ns/1ps;
-<<<<<<< HEAD
+
 module sc_safety_monitor
 (
 	input logic clk,
@@ -62,58 +62,13 @@ module sc_safety_monitor
 				end
 				default: begin
 				    fault_flag_comb = 0;
-=======
-`include "rtl/include/sc_include.svh"
-
-module sc_safety_monitor
-(
-	input logic reset_n, 
-	
-	sc_interface_if.safety safety_bus
-);
-	import sc_types_pkg::*;
-	
-	//combinational logic to state conditions
-	always_comb begin
-		//default
-		safety_bus.fault_flag = 0;
-		safety_bus.fault_code = '0;
-		
-		grd_saft = grid_state_t'(safety_bus.grid_state);
-		
-		unique case(safety_bus.current_state)
-			
-			IDLE: begin
-			   safety_bus.fault_flag = 0;
-			   safety_bus.fault_code = 2'b00;
-			end
-			
-			CHECK_GRID: begin
-			    case(grd_saft)
-				GRID_CRITICAL: begin
-				    safety_bus.fault_flag = 1;
-				    safety_bus.fault_code = 2'b11;
-				end
-				
-				GRID_UNSTABLE: begin
-				    safety_bus.fault_flag = 0;
-				    safety_bus.fault_code = 2'b10;
-				end
-				
-				GRID_NORMAL: begin 
-				    safety_bus.fault_flag = 0;
-				    safety_bus.fault_code = 2'b01;			
-				end
-				default: begin
-				    safety_bus.fault_flag = safety_bus.ml_predict_instability;
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 				end
 			    endcase
 			end
 
 			CHARGING: begin
 				//grid verification
-<<<<<<< HEAD
+				
 				case(grid_state_enum)
 				   GRID_CRITICAL: begin
 				     fault_flag_comb = 1'b1;
@@ -130,30 +85,11 @@ module sc_safety_monitor
 				   
 				   default: begin
 					fault_flag_comb = safety_bus.ml_predict_instability;
-=======
-				case(grd_saft)
-				   GRID_CRITICAL: begin
-				     safety_bus.fault_flag = 1'b1;
-				     safety_bus.fault_code = 2'b11;
-				   end
-				   GRID_UNSTABLE: begin
-					safety_bus.fault_flag = 1'b0;
-				        safety_bus.fault_code = 2'b10;
-				   end
-				   GRID_NORMAL: begin
-				    safety_bus.fault_flag = 1'b0;
-				    safety_bus.fault_code = 2'b01;
-				   end
-				   
-				   default: begin
-					safety_bus.fault_flag = safety_bus.ml_predict_instability;
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 				   end
 			    	endcase
 			end
 			
 			WAIT: begin
-<<<<<<< HEAD
 			   
 			   case(grid_state_enum)
 				   GRID_CRITICAL: begin
@@ -205,39 +141,10 @@ module sc_safety_monitor
 				
 				if(!reset_n) begin
 				    fault_flag_comb = 0;
-=======
-			    
-			   if(grd_saft == GRID_UNSTABLE || grd_saft == GRID_NORMAL)
-			   begin
-				safety_bus.fault_flag = 0;
-				safety_bus.fault_code = 2'b10;
-			   end
-			end
-
-			FAULT: begin
-				if(grd_saft == GRID_CRITICAL) begin
-				   safety_bus.fault_flag = 1;
-				   safety_bus.fault_code = 2'b11;
-				end
-				
-				else if(grd_saft == GRID_NORMAL) begin
-				   safety_bus.fault_flag = 1'b0;
-				   safety_bus.fault_code = 2'b01;
-				end
-				
-				else if(grd_saft == GRID_UNSTABLE) begin
-				   safety_bus.fault_flag = 1'b0;
-				   safety_bus.fault_code = 2'b10;
-				end
-				
-				if(!reset_n) begin
-				    safety_bus.fault_flag = 0;
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 				end
 			end
 			
 			default: begin
-<<<<<<< HEAD
 				fault_flag_comb = safety_bus.ml_predict_instability;
 			end
 	       endcase
@@ -258,15 +165,6 @@ module sc_safety_monitor
 	    end
 	end
 	*/
-=======
-				safety_bus.fault_flag = safety_bus.ml_predict_instability;
-			end
-	       endcase
-	end
-	
-	
-	//outputs(assigns)
->>>>>>> cc3f88259f336a35c3ba70f2c756ca125d91aee8
 	
 endmodule
 					
